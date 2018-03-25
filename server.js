984453d54beda7d6211bd4d47572310a3a4eae6a
Tara Fenton <tarafenton@Taras-MBP.fios-router.home>
2018-03-25 11:14:38 -0400

    before clean up

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const session = require("express-session");
 const User = require("./models/User");
 const Venue = require("./models/Venue");
 
+const spotifyApp = require("./spotify-api");
+
+app.use(spotifyApp);
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 require("es6-promise").polyfill();
 require("isomorphic-fetch");
@@ -28,7 +31,7 @@ var spotifyApi = new SpotifyWebApi({
   redirectUri: "http://localhost:8888/callback"
 });
 spotifyApi.setAccessToken(
-  "BQBBOzAkZYjdlI_QkKU26PMEXg6BSO3xFZME-PMqzQR6YaDa3lEBjAYY-pjL08g4klrDHGYyDYUO3XeR6nyTronToz__xiYrQxjRQ0nMdwWLXTQ4AHB6oORVU8azowb8FtzhZuWkrCCZZavE_KFe2UXJSB25RtXZrQ&refresh_token=AQDoeEi6hwfs5IfTiluUpe5vmXwqLi5mEv2CjetMCtt10Qex2gtKHuimU67eL51o4DohNdqOV6SB3jY3EoZaYJGZsd8poK-Xhmm11lRg714yZRomIYnuXGrj1qQtF-7nFbw"
+  "BQD8-fNdQGGYb9o2OIFUICyg2kD45fJaRMJw41dKaSPX16URkKUCJ_EA57O-215iuqg2g3RC1iuQGa8kbGO4ZnCV0GGPe1LtwQHARchxuU_Rwfwb-SHAy647bgyVHmYk9J9RAx6e8aOUTBsbEHVBKBdjy0UqUKN46A&refresh_token=AQBmfXEu3xOP1FfMiZMczttl9D8-jf5VEEuL7glVzt7GA1RN6QwKhufbce2YF-Pj_zBPoqoiNb1YzMk4v2KkCWOkjC6zDuWxxx3h0MPBlTzqzcWfUR7JMADJjf7NAyKt04o"
 );
 // Set up the session middleware which will let use `request.session`
 app.use(
@@ -63,12 +66,14 @@ app.get("/", requireLogin, (request, response) => {
 });
 
 let message = "";
-/////////////////////////////////////////////////////////// LOG IN ////////
+// ///////////////////////////////////////////////////////// LOG IN ////////
 // Render a login form for the user.
 app.get("/login", (request, response) => {
   response.render("login", { message });
 });
-
+app.get("/spotify-login", (request, response) => {
+  response.render("spotify-login");
+});
 // Login the user if their username and password are correct.
 app.post("/login", urlencodedParser, (request, response) => {
   message = "";
@@ -88,7 +93,8 @@ app.post("/login", urlencodedParser, (request, response) => {
       if (request.body.username === user.user_name && isMatch) {
         // Set the session data.
         request.session.authenticated = true;
-        response.redirect("/");
+        // response.redirect("/");
+        response.render("spotify-login");
       } else {
         // password does not match
         message = "sorry password does not match with username";
@@ -99,7 +105,7 @@ app.post("/login", urlencodedParser, (request, response) => {
       response.send(error);
     });
 });
-/////////////////////////////////////////////////////////// SIGN UP ///////
+// ///////////////////////////////////////////////////////// SIGN UP ///////
 // Render a signup form for the user.
 app.get("/signup", (request, response) => {
   message = "";
@@ -120,35 +126,100 @@ app.post("/signup", urlencodedParser, (request, response) => {
       response.send(error);
     });
 });
-///////////////////////////////////////////////////// VENUE LIST ////////
+// /////////////////////////////////////////////////// VENUE LIST ////////
 // Render a venue list page for the user to select a venue page to go to
 app.get("/venue-list", (request, response) => {
   Venue.findAll().then(venues => {
+    request.session.access_token = request.query.access_token;
+    console.log(
+      "request.query.access_token in venue list ",
+      request.session.access_token
+    );
     response.render("venue-list", { venues: venues });
     //response.json(venues);
   });
 });
 // https://api.spotify.com/v1/users/tarafenton/playlists
-///////////////////////////////////////////////////// VENUE PAGE ////////
-
+// /////////////////////////////////////////////////// VENUE PAGE ////////
+const getReponseAsJSON = url => {
+  return fetch(url).then(response => response.json());
+};
 // Render a venue page for the user to play
 app.get("/venue/", (request, response) => {
   const venueName = request.query.venue;
-  const sendData = { venue: venueName };
-  // Get Elvis' albums
-  spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE").then(
+
+  // console.log(access_token);
+  // const sendData = { venue: venueName };
+  getReponseAsJSON(
+    `https://api.spotify.com/v1/users/tarafenton/playlists/?access_token=${
+      request.session.access_token
+    }`
+  ).then(json => {
+    console.log(json);
+    response.render("venue", { venueName: venueName, data: json });
+  });
+  // fetch(
+  //   `https://api.spotify.com/v1/users/tarafenton/playlists/?access_token=${
+  //     request.session.access_token
+  //   }`
+  // )
+  //   .then(data => {
+  //     if (response.status >= 400) {
+  //       throw new Error("Bad response from server");
+  //     }
+  //     console.log(data);
+  //     response.render("venue", { venueName: venueName, data: data });
+  //     // return response.json();
+  //   })
+  //   .catch(error => {
+  //     response.send(error);
+  //   });
+});
+
+// get users playlists
+// Get a user's playlists
+// spotifyApi.getUserPlaylists("tarafenton").then(
+//   function(data) {
+//     console.log("Retrieved playlists", data.body);
+//     sendData.data = data.body;
+//     response.render("venue", { venueName: venueName });
+//   },
+//   function(err) {
+//     console.log("Something went wrong!", err);
+//   }
+// );
+
+// Get Elvis' albums
+// spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE").then(
+//   function(data) {
+//     sendData.data = data.body;
+//     console.log("Artist albums", data.body);
+//     response.render("venue", { sendData: sendData });
+//   },
+//   function(err) {
+//     console.error(err);
+//   }
+// );
+
+app.get("/playlist/", (request, response) => {
+  const playlistId = request.query.playlistId;
+  //const sendData = { venue: venueName };
+
+  // get users playlists
+  // Get a user's playlists
+  // Get a playlist
+  spotifyApi.getPlaylist("tarafenton", playlistId).then(
     function(data) {
-      sendData.data = data.body;
-      console.log("Artist albums", data.body);
-      response.render("venue", { sendData: sendData });
+      console.log("Some information about this playlist", data.body);
+
+      response.render("venue", { sendData: data.body });
     },
     function(err) {
-      console.error(err);
+      console.log("Something went wrong!", err);
     }
   );
 });
-
-////////////////////////////////////////////////////// LISTEN TO PORT ////
+// //////////////////////////////////////////////////// LISTEN TO PORT ////
 // listen for port
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
