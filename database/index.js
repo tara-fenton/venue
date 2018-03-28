d3d41f70d5393444e65de9d9e7e0670606e227ea
Tara Fenton <tarafenton@Taras-MBP.fios-router.home>
2018-03-28 18:59:30 -0400

    database connection

--- database/index.js
+++ database/index.js
@@ -6,13 +6,13 @@ let db;
 if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
   db = pgp({
     // Fill in with your local database name
-    database: "deohajts1prh5t",
+    database: "venue_app",
     port: 5432,
     host: "localhost"
   });
 } else if (process.env.NODE_ENV === "production") {
   // Heroku will set this variable for you.
-  db = pgp(process.env.DATABASE_URL);
+  db = pgp(`${process.env.DATABASE_URL}?ssl=true`);
 }
 
 module.exports = db;
